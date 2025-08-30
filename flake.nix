a688db327965d0d746dbfee14ebcc150733478d6
Sinh Nguyen <sinh@sinhn.com>
2025-08-30 19:31:05 +0700

    update flake

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs-python.url = "github:NixOS/nixpkgs/nixos-23.11";
     zmk-nix = {
       url = "github:lilyinstarlight/zmk-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -13,6 +13,7 @@
       self,
       nixpkgs,
       zmk-nix,
+      nixpkgs-python,
     }:
     let
       forAllSystems = nixpkgs.lib.genAttrs (nixpkgs.lib.attrNames zmk-nix.packages);
@@ -23,7 +24,6 @@
 
         firmware = zmk-nix.legacyPackages.${system}.buildSplitKeyboard {
           name = "firmware";
-
           src = nixpkgs.lib.sourceFilesBySuffices self [
             ".board"
             ".cmake"
@@ -38,12 +38,9 @@
             ".yml"
             "_defconfig"
           ];
-
           board = "nice_nano_v2";
           shield = "corne_%PART%";
-
-          zephyrDepsHash = "";
-
+          zephyrDepsHash = "sha256-79/rYCtUDlC0K4ARO9MSEaCcI1RQSsv7MCeayVZSwtQ=";
           meta = {
             description = "ZMK firmware";
             license = nixpkgs.lib.licenses.mit;
@@ -59,16 +56,18 @@
         system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
+          pkgs-python = nixpkgs-python.legacyPackages.${system};
         in
         {
           default = zmk-nix.devShells.${system}.default.overrideAttrs (oldAttrs: {
             buildInputs = (oldAttrs.buildInputs or [ ]) ++ [
-              pkgs.python310
-              pkgs.python310Packages.setuptools
-              pkgs.python310Packages.protobuf
+              pkgs-python.python310
+              pkgs-python.python310Packages.setuptools
+              pkgs-python.python310Packages.protobuf
             ];
           });
         }
       );
     };
+
 }
